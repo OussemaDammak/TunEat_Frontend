cefd415f32bf33522ee98cfd328ffdbb4fbfce06
Oussema Dammak <oussemadammak01@gmail.com>
2025-10-18 11:56:04 +0100

    setup between front and back

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
+"use client";
+
+import axios from "axios";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constants";
+
+import { Search } from "lucide-react";
 import Image from "next/image";
-
+import { useState, useEffect } from "react";
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [user, setUser] = useState<UserType | null>(null);
+  const [recipes, setRecipes] = useState<RecipeType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState<string | number>(
+    "all"
   );
+
+  //Modal states
+
+  const [showAuth, setShowAuth] = useState(false);
+  const [showAddRecipe, setShowAddRecipe] = useState(false);
+  const [loadingRecipes, setLoadingRecipes]= useState(false);
+  const [viewingRecipe, setViewingRecipe] = useState<RecipeType | null>(null);
+  const [showUserProfile, setShowUserProfile] = useState(false);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        setLoadingRecipes(true)
+        const res = await axios.get<RecipeType[]>(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/recipes/`,
+          {
+            params: {
+              search: searchTerm,
+              category:
+                selectedCategory !== "all" ? selectedCategory : undefined,
+            },
+          }
+        );
+        setRecipes(res.data);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }finally{
+        setLoadingRecipes(false)
+      }
+    };
+
+    fetchRecipes();
+  }, [searchTerm, selectedCategory]);
+  useEffect(() => {
+    fetchCategories();
+    fetchUser();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const res = await axios.get<CategoryType[]>(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/categories/`
+      );
+      setCategories(res.data);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+
+  const fetchUser = async () => {
+    try {
+   
+         const res = await axios.get<UserType>(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/profile/`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem(ACCESS_TOKEN)}`,
+          },
+        }
+      );
+      setUser(res.data);
+    
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  };
+
+    console.log("avalable recipes: ",recipes);
+
+
 }
